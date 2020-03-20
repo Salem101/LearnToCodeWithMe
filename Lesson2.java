22ab4b0d9aa6e3e7255ea72d6b0db29da9056933
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2020-03-20 00:38:05 -0400

    Create Lesson2.java

---
+++ Lesson2.java
@@ -0,0 +1,23 @@
+//You need to import the scanner to be able to use it to get data from user
+import java.util.Scanner; 
+
+//public means its visible to the public (it's not private)
+public class IOchallenge {
+
+	public static void main (String[]args) {
+		//use the scanner object to get info from the user
+		Scanner scan = new Scanner(System.in);
+		//print a question 
+		System.out.println("What's your first number?"); 
+		//'a' will hold the answer that the user typed in 
+        int a = scan.nextInt();
+		System.out.println("What's your second number?"); 
+        int b = scan.nextInt(); 
+		System.out.println("What's your third number?"); 
+        int c = scan.nextInt(); 
+        // use the + sign to write separate variables in the same line
+        //putting " " around words makes them strings 
+        System.out.println("Numbers Chosen: " +  a + " " + b + " " + c ); 
+
+    }
+}
