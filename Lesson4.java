a2138a4bb1b79208e7561d793376d95e9422c85f
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2020-05-07 00:05:26 -0400

    Create Lesson4.java

---
+++ Lesson4.java
@@ -0,0 +1,22 @@
+
+public class starPattern {
+
+	public static void main(String[]args) {
+/*objective: Display the following star pattern
+		 *
+		***
+	   *****
+		***
+     	 **
+		 *
+		 */
+    //This is really simple just to show you how spacing connects to things that are displayed
+System.out.println("  *  "); 
+System.out.println(" *** "); 
+System.out.println("*****"); 
+System.out.println(" *** "); 
+System.out.println("  *  "); 
+
+
+	}
+}
